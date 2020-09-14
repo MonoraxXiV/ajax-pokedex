dcc8523a2d063bfde362b7ebc72b51bdbc3eb868
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-14 12:22:51 +0200

    pre-lunch commit, still trying to get data to display currently seeing undefined for those I do show

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
 //https://www.taniarascia.com/how-to-connect-to-an-api-with-javascript/ resource to help find the API content
 //https://pokeapi.co/docs/v2#pokemon
 
-
-var searchButton= document.getElementById("run").addEventListener("click",function() {
+let pokemon = [];
+var searchButton = document.getElementById("run").addEventListener("click", function () {
 
 
     function fetchPokemon() {
-        var input= document.getElementById("pokemonID").value;
+        var input = document.getElementById("pokemonID").value;
         console.log(input); //already getting data based on number and ID, now to show the needed data.
         fetch('https://pokeapi.co/api/v2/pokemon/' + input)
             .then((response) => {
                 return response.json()
             })
-            .then(data=> {
+            .then(data => {
                 console.log(data);
+                pokemon = data;
             })
             .catch((err) => {
                 console.error("Pokemon not found");
             })
     }
 
+    fetchPokemon()
+    let templateNode = document.getElementById('tpl-pokemon').content.cloneNode(true);
+    templateNode.querySelector(".name").innerHTML = pokemon.name;
+    templateNode.querySelector(".id").innerHTML = pokemon.id;
+    templateNode.querySelector(".evolution").innerHTML = "evolves to: " + pokemon.evolves_to;
+    document.getElementById("target").appendChild(templateNode);
     /*
     //basically we want to put the data in this template that we made
     function displayPokemon(pokemon) {
@@ -39,6 +46,5 @@ var searchButton= document.getElementById("run").addEventListener("click",functi
 
     }
     */
-     fetchPokemon()
 
 })
