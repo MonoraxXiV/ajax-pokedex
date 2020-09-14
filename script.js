7de1c6a2d6dddb75a2eccbd3aea7f094f852c19c
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-14 15:41:13 +0200

    pokedex shows now 1 move

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let pokemon = [];
 var searchButton = document.getElementById("run").addEventListener("click", function () {
 
 
+
     function fetchPokemon() {
         var input = document.getElementById("pokemonID").value;
         console.log(input); //already getting data based on number and ID, now to show the needed data.
@@ -14,20 +15,28 @@ var searchButton = document.getElementById("run").addEventListener("click", func
             })
             .then(data=> displayPokemon(data))
             .catch((err) => {
-                console.error("unable to fetch");
+                alert("unable to fetch, please use a name or id");
             })
     }
     fetchPokemon()
-    function displayPokemon(data) {
+
+
+
+   function displayPokemon(data) {
 
         let templateNode = document.getElementById('tpl-pokemon').content.cloneNode(true);
         templateNode.querySelector(".name").innerHTML = data.name;
         console.log(data.name);
         templateNode.querySelector(".id").innerHTML = data.id;
         templateNode.querySelector(".evolution").innerHTML = "evolves to: ";
+        //save as an array. display four elements(move.name)!!watch out for ditto and smeargle.
+
+       moves=data.moves[1].move.name;
+        templateNode.querySelector(".moves").innerHTML=""+moves;
         //still need to limit moves to 4 random ones.
         //separate API call for evolutions.
         document.getElementById("target").appendChild(templateNode);
     }
+
 displayPokemon()
 })
