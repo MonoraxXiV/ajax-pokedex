e62c3a912993d7336cf29dc8d9202484fbd4c3ae
Kayalin Van Kogelenberg <kayalin.van.kogelenberg@gmail.com>
2020-09-15 15:08:51 +0200

    now shows 4 moves, trying to clear templateNode.innerhtml so it stops adding pokemon instead of replacing

--- script.js
+++ script.js
@@ -2,11 +2,10 @@
 //https://pokeapi.co/docs/v2#pokemon
 
 let pokemon = [];
-var searchButton = document.getElementById("run").addEventListener("click", function () {
+document.getElementById("run").addEventListener("click", function () {
 
 
-
-    function fetchPokemon() {
+    async function fetchPokemon() {
 
         var input = document.getElementById("pokemonID").value;
         console.log(input); //already getting data based on number and ID, now to show the needed data.
@@ -14,49 +13,71 @@ var searchButton = document.getElementById("run").addEventListener("click", func
             .then((response) => {
                 return response.json()
             })
-            .then(data=> displayPokemon(data))
-            .then(data=> {
-                return fetch('https://pokeapi.co/api/v2/pokemon-species/'+data.id)
-            })
-            .then(function(response) {
-                return response.json();
-            })
-            .then(function(data) {
-                // do stuff with `data`
-            })
-            .catch(function(error) {
-                console.log('Requestfailed', error)
-            });
+            .then(data => displayPokemon(data))
+
 
     }
+
     fetchPokemon()
 
-   function displayPokemon(data) {
+    function displayPokemon(data) {
 
         let templateNode = document.getElementById('tpl-pokemon').content.cloneNode(true);
+        //loop over template node and empty innerhtml
+        // for every child in template node ="";
+
         templateNode.querySelector(".name").innerHTML = data.name;
         console.log(data);
         templateNode.querySelector(".id").innerHTML = data.id;
         templateNode.querySelector(".evolution").innerHTML = "evolves to: ";
         //save as an array. display four elements(move.name)!!watch out for ditto and smeargle.
-            var i=0;
+        var i = 0;
+        if (data.moves.length === 1) {
 
-            for (i; i<4; i++) {
+            templateNode.querySelector(".moves").innerHTML = " " + data.moves[0].move.name;
+        } else {
+            for (i; i < 4; i++) {
 
-                moves = data.moves[i].move.name;
-                templateNode.querySelector(".moves").innerHTML = "" + moves;
+                let movePokemon = data.moves[i];
+                templateNode.querySelector(".moves").innerHTML +=" "+movePokemon.move.name+",";
             }
-           let sprites= data.sprites.front_default;
-       document.getElementById("placeholder").src=sprites;
+        }
+        let sprites = data.sprites.front_default;
+        document.getElementById("placeholder").src = sprites;
         //still need to limit moves to 4 random ones.
         //separate API call for evolutions.
+
         document.getElementById("target").appendChild(templateNode);
+        for (i; i< templateNode.childNodes.length; i++){
 
-
+            templateNode.innerHTML = '';
+        }
 
 
 
     }
 
-displayPokemon()
+
+        fetch('https://pokeapi.co/api/v2/pokemon-species/' + input)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                return fetch(data.evolution_chain)
+            })
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data.evolution);
+            })
+            .catch(function (error) {
+                console.log('Requestfailed', error)
+            });
+
+
+
+
+    displayPokemon()
+
 })
